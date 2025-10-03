2b54287d7476fe22944437a6884d9a5f5f431e95
tayo <cetydistjoy@gmail.com>
2025-10-03 11:38:33 +0100

    Initial Commit

---
+++ HelloApple/HelloAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleApp.swift
+//  HelloApple
+//
+//  Created by Tayo Lanre on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
